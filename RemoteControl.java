8815e49d7adf5d0c1e18fb9f1044aff1fc31b6e3
Theepag <theepag@live.com>
2020-08-12 21:44:13 +0530

    Update RemoteControl.java

--- RemoteControl.java
+++ RemoteControl.java
@@ -1,7 +1,7 @@
-package normal;
+
 //Basic Remote Control (turnOn , turnOff)
 //Advance Remote control (setChannel)
-//Movie Remote control (Play, pause , rewind)
+
 
 public abstract class RemoteControl {
     public abstract void turnOn();
